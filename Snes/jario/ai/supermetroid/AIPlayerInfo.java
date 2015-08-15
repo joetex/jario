c0227447dc27ece7dd801bd26bb5403e94ba74d8
OneEyed <joelruiz2@gmail.com>
2015-08-15 03:09:59 -0500

    Begun mapping memory (game data) to an API for easier usage

--- Snes/jario/ai/supermetroid/AIPlayerInfo.java
+++ Snes/jario/ai/supermetroid/AIPlayerInfo.java
@@ -13,52 +13,154 @@ public class AIPlayerInfo implements Hardware {
 		
 	}
 	
-	public int getHealth() {
+	public void dumpRam()
+	{
+		
+	}
+	public int getRoomTime()
+	{
 		return 0;
 	}
 	
-	public int getEnergy() {
-		return 0;
+	/*
+	 * Game state. 01 = Title screen, 04 = menus, 05 = Load area, 
+	 * 06 = Loading game, 07 = Samus being electrified in save capsule, 
+	 * 08 = normal gameplay, 0C = pausing, 0F = paused, 
+	 * 12 = unpausing, 15, 17, 18, 19, 1A = Dead or dying, 
+	 * 23 = Timer up. 24 = blackout and gameover (Ceres explodes if not escaping Zebes). 
+	 * 2A = intro demos
+	 */
+	public int gameState()
+	{
+		return memorybus.read8bit(0x7E0998);
 	}
 	
-	public int getMissleCount() {
-		return 0;
+	public int isElevatorTransition()
+	{
+		return memorybus.read8bit(0x7E0E16);
 	}
 	
-	public int getSuperMissleCount() {
-		return 0;
+	public int isRoomTransition()
+	{
+		return memorybus.read8bit(0x7E0797);
 	}
 	
-	public int getPowerBombCount() {
-		return 0;
+	public Vector2D roomBlockSize()
+	{
+		int w = memorybus.read8bit(0x7E07A5);
+		int h = memorybus.read8bit(0x7E07A7);
+		return new Vector2D(w,h);
 	}
 	
-	public int getBombCount() {
-		return 0;
+	public int getRoomID()
+	{
+		return memorybus.read8bit(0x7E079D);
 	}
 	
-	public int getCurrentMode() {
-		return 0;
+	public int getRegionNumber()
+	{
+		return memorybus.read8bit(0x7E079F);
 	}
 	
-	public int getCurrentSuit() {
-		return 0;
+	public int getRoomEnemyCount()
+	{
+		return memorybus.read8bit(0x7E0E4E);
 	}
 	
-	public int getCurrentWeapon() {
-		return 0;
+	public int getRoomEnemiesKilled()
+	{
+		return memorybus.read8bit(0x7E0E50);
 	}
 	
-	public Vector2D getPosition() {
-		return null;
+	public int getRoomKillsNeeded()
+	{
+		return memorybus.read8bit(0x7E0E52);
 	}
 	
-	public Vector2D getVelocity() {
-		return null;
+	public int getEnergy() 
+	{
+		return memorybus.read8bit(0x7E09C2);
+	}
+	
+	public int getMissleCount() 
+	{
+		return memorybus.read8bit(0x7E09C6);
+	}
+	
+	public int getSuperMissleCount() 
+	{
+		return memorybus.read8bit(0x7E09CA);
+	}
+	
+	public int getPowerBombCount() 
+	{
+		return memorybus.read8bit(0x7E09CE);
+	}
+	
+	public int getBombCount()
+	{
+		return memorybus.read8bit(0x7E0CD2);
+	}
+	
+	public int getSelectedItem()
+	{
+		return memorybus.read8bit(0x7E09D2);
+	}
+	
+	public int getCurrentMode() 
+	{
+		return memorybus.read8bit(0x7E09A2);
+	}
+	
+	public int getCurrentSuit()
+	{
+		return memorybus.read8bit(0x7E09A2);
+	}
+	
+	public int getCurrentWeapon() 
+	{
+		return memorybus.read8bit(0x7E09A6);
+	}
+	
+	public Vector2D getPosition() 
+	{
+		int x = memorybus.read8bit(0x7E0AF8);
+		int y = memorybus.read8bit(0x7E0AFC);
+		return new Vector2D(x,y);
+	}
+	
+	//Position/State?
+	public int getState()
+	{
+		return memorybus.read8bit(0x7E0A1C);
+	}
+	
+	public int getMovementType()
+	{
+		int movementtype = memorybus.read8bit(0x7E0A1F);
+		return (movementtype & 0xFF);
+	}
+	
+	public Vector2D getVelocity() 
+	{
+		int x = memorybus.read8bit(0x7E0B44);
+		int y = memorybus.read8bit(0x7E0B2C);
+		return new Vector2D(x,y);
+	}
+	
+	
+	//X = horizontal momentum
+	//Y = vertical acceleration
+	public Vector2D getMomentum()
+	{
+		int x = memorybus.read8bit(0x7E0B48);
+		int y = memorybus.read8bit(0x7E0B32);
+		return new Vector2D(x,y);
 	}
 
 	@Override
-	public void connect(int port, Hardware hw) {
+	public void connect(int port, Hardware hw) 
+	{
 		// TODO Auto-generated method stub
 		switch(port)
 		{
